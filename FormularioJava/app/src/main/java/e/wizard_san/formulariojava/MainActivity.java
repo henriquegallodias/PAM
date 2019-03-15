85ec554c7e38812807f333522aff70c82e23c33a
henriquegallodias <henrique0521@hotmail.com>
2019-03-14 22:33:53 -0300

    formulario validado em linguagem java , criado no android studios

---
+++ FormularioJava/app/src/main/java/e/wizard_san/formulariojava/MainActivity.java
@@ -0,0 +1,43 @@
+package e.wizard_san.formulariojava;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText USER;
+    private EditText PASS;
+    private Button BTN;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        USER = (EditText) findViewById(R.id.login);
+        PASS = (EditText) findViewById(R.id.senha);
+        BTN = (Button) findViewById(R.id.btn);
+
+        BTN.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               String txt = USER.getText().toString();
+               String pass = PASS.getText().toString();
+               String henrique = "henrique";
+               String senhaa = "123456";
+
+               if(txt.equals(henrique)&& pass.equals(senhaa)) {
+                   Toast.makeText(MainActivity.this, "login aceito", Toast.LENGTH_LONG).show();
+               }
+               else {
+                   Toast.makeText(MainActivity.this,"usuário ou senha incorretos",Toast.LENGTH_LONG).show();
+               }
+           }
+        });
+
+    }
+}
